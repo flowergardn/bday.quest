65871bceb395be014eb176d0950fc70efe0b29e8
flowergardn <me@astrid.sh>
2025-08-19 15:42:17 -0400

    chore: clean up stats component

--- src/components/statistics.tsx
+++ src/components/statistics.tsx
@@ -4,23 +4,19 @@ import { sql } from "drizzle-orm";
 import { db } from "~/server/db";
 import { cards, wishes } from "~/server/db/schema";
 
-async function countRows(table: typeof cards | typeof wishes) {
-  const [value] = await db.select({ count: sql<number>`count(*)` }).from(table);
-  if (value) return value.count;
-  return 0;
+async function countRows(table: typeof cards | typeof wishes): Promise<string> {
+  const [value] = await db
+    .select({ count: sql<number>`count(id)` })
+    .from(table);
+  if (value) return Intl.NumberFormat("en-US").format(value.count);
+  return "0";
 }
 
 const Statistics = async () => {
-  const cardStats = await countRows(cards);
-  const wishStats = await countRows(wishes);
-
-  const cardCount = Intl.NumberFormat("en-US").format(cardStats);
-  const wishCount = Intl.NumberFormat("en-US").format(wishStats);
-
   return (
     <p className="text-center">
-      So far, <strong>{cardCount}</strong> cards have been created, and{" "}
-      <strong>{wishCount}</strong> wishes have been collected.
+      So far, <strong>{await countRows(cards)}</strong> cards have been created,
+      and <strong>{await countRows(wishes)}</strong> wishes have been collected.
     </p>
   );
 };
